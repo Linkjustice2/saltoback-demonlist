a8e8267037a2e40a1f33630405ff672cf131ef3c
Linkjustice2 <gdbacgdbac@gmail.com>
2025-08-13 22:41:26 -0500

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -2,6 +2,7 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import CList from './pages/CList.js'; // <-- new page
+import ChallengeLeaderboard from './pages/ChallengeLeaderboard.js';
 
 export default [
   { path: '/', component: List },
